68b437ce28c7e0de67249138ea53bae83d364bff
PabloG03 <pablo03grp@gmail.com>
2024-11-08 18:11:42 +0000

    Comentado la clase WildCard

--- src/main/java/domain/card/WildCard.java
+++ src/main/java/domain/card/WildCard.java
@@ -2,6 +2,10 @@ package domain.card;
 
 import java.util.Objects;
 
+/**
+ * Clase que representa una carta comodín.
+ * Implementa la interfaz Card.
+ */
 public class WildCard extends AbstractCard {
     public WildCard(CardType type) {
         super(type, null);
@@ -12,6 +16,11 @@ public class WildCard extends AbstractCard {
         CardUtil.validateColor(color);
     }
 
+    /**
+     * Comprueba si la carta es válida para ser jugada.
+     * @param topCard carta superior de la pila.
+     * @return true si la carta es válida, false en caso contrario.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -20,11 +29,19 @@ public class WildCard extends AbstractCard {
         return getType() == wildCard.getType() && getColor() == wildCard.getColor();
     }
 
+    /**
+     * Devuelve el hash code de la carta.
+     * @return hash code de la carta.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getType(), getColor());
     }
 
+    /**
+     * Devuelve la representación en cadena de la carta.
+     * @return representación en cadena de la carta.
+     */
     @Override
     public String toString() {
         return "WildCard{" +
