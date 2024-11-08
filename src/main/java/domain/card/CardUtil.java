369cacc11f7eb128c86281e0b2bffa49428085ca
PabloG03 <pablo03grp@gmail.com>
2024-11-08 18:10:12 +0000

    Comentado la clase CardUtil

--- src/main/java/domain/card/CardUtil.java
+++ src/main/java/domain/card/CardUtil.java
@@ -1,21 +1,36 @@
 package domain.card;
 
+/**
+ * Clase de utilidad para validar las propiedades de las cartas.
+ */
 public class CardUtil {
     private CardUtil() {
     }
 
+    /**
+     * Valida el color de una carta.
+     * @param color Color de la carta.
+     */
     public static void validateColor(CardColor color) {
         if (color == null) {
             throw new IllegalArgumentException("Card color should be defined");
         }
     }
 
+    /**
+     * Valida el número de una carta.
+     * @param number Número de la carta.
+     */
     public static void validateNumber(int number) {
         if (number < 0 || number > 9) {
             throw new IllegalArgumentException("Card number should between 0 and 9");
         }
     }
 
+    /**
+     * Valida el tipo de una carta de acción.
+     * @param type Tipo de la carta de acción.
+     */
     public static void validateActionType(CardType type) {
         if (type == CardType.SKIP || type == CardType.REVERSE || type == CardType.DRAW_TWO) {
             return;
@@ -24,6 +39,10 @@ public class CardUtil {
         throw new IllegalArgumentException("Invalid action type");
     }
 
+    /**
+     * Valida el tipo de una carta comodín.
+     * @param type Tipo de la carta comodín.
+     */
     public static boolean isWildCard(Card card) {
         return card.getType() == CardType.WILD_COLOR || card.getType() == CardType.WILD_DRAW_FOUR;
     }
