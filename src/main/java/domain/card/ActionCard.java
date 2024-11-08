6fe31c5b74c15f86b4c7a36672b1ae71d2b23f66
PabloG03 <pablo03grp@gmail.com>
2024-11-08 18:03:28 +0000

    Comentada la clase ActionCard

--- src/main/java/domain/card/ActionCard.java
+++ src/main/java/domain/card/ActionCard.java
@@ -2,6 +2,10 @@ package domain.card;
 
 import java.util.Objects;
 
+/**
+ * Clase que representa una carta de acción.
+ * Implementa la interfaz Card.
+ */
 public class ActionCard extends AbstractCard {
     public ActionCard(CardType type, CardColor color) {
         super(type, color);
@@ -9,6 +13,11 @@ public class ActionCard extends AbstractCard {
         CardUtil.validateColor(color);
     }
 
+    /**
+     * Comprueba si la carta es válida para ser jugada.
+     * @param topCard carta superior de la pila.
+     * @return true si la carta es válida, false en caso contrario.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -17,11 +26,19 @@ public class ActionCard extends AbstractCard {
         return getType() == that.getType() && getColor() == that.getColor();
     }
 
+    /**
+     * Devuelve el hash code de la carta.
+     * @return hash code de la carta.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getType(), getColor());
     }
 
+    /**
+     * Devuelve la representación en cadena de la carta.
+     * @return representación en cadena de la carta.
+     */
     @Override
     public String toString() {
         return "ActionCard{" +
