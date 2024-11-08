8746d4010c87cf465d24ec95ab8d460cb98bd113
PabloG03 <pablo03grp@gmail.com>
2024-11-08 18:22:33 +0000

    Comentado la clase AbstractCard

--- src/main/java/domain/card/AbstractCard.java
+++ src/main/java/domain/card/AbstractCard.java
@@ -1,5 +1,10 @@
 package domain.card;
 
+/**
+ * Clase abstracta que implementa la interfaz Card.
+ * Define los métodos getType y getColor.
+ * Implementa los métodos equals, hashCode y toString.
+ */
 public abstract class AbstractCard implements Card {
     private final CardType type;
     private final CardColor color;
@@ -9,22 +14,43 @@ public abstract class AbstractCard implements Card {
         this.color = color;
     }
 
+    /**
+     * Devuelve el tipo de la carta.
+     * @return tipo de la carta.
+     */
     @Override
     public CardType getType() {
         return type;
     }
 
+    /**
+     * Devuelve el color de la carta.
+     * @return color de la carta.
+     */
     @Override
     public CardColor getColor() {
         return color;
     }
 
+    /**
+     * Comprueba si dos cartas son iguales.
+     * @param o objeto a comparar.
+     * @return true si las cartas son iguales, false en caso contrario.
+     */
     @Override
     public abstract boolean equals(Object o);
 
+    /**
+     * Devuelve el hash code de la carta.
+     * @return hash code de la carta.
+     */
     @Override
     public abstract int hashCode();
 
+    /**
+     * Devuelve la representación en cadena de la carta.
+     * @return representación en cadena de la carta.
+     */
     @Override
     public abstract String toString();
 }
