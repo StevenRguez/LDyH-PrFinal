205e70cc1bc711b126dd2d992ec346e5324fd71e
mariaferrandizdiaz <mariaferrandizdiaz2003@gmail.com>
2024-12-06 19:41:52 +0000

    Added tests for class main/java/domain/player/InmutablePlayer

---
+++ src/test/java/domain/player/TestInmutablePlayer.java
@@ -0,0 +1,38 @@
+package domain.player;
+
+import domain.card.Card;
+import domain.card.CardColor;
+import domain.card.CardType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Pruebas unitarias para la clase ImmutablePlayer.
+ */
+class ImmutablePlayerTest {
+    // Test para la clase de Inmutable Player que se encarga de devolver el identificador del jugador.
+    @Test
+    void testGetId() {
+        // Creamos un jugador con un identificador aleatorio.
+        var player = new Player("TestPlayer", new HandCardList());
+        var immutablePlayer = new ImmutablePlayer(player);
+        // Comprobamos que el identificador del jugador es el mismo que el del jugador inmutable.
+        assertEquals(player.getId(), immutablePlayer.getId());
+    }
+
+    // Test para la clase de Inmutable Player que se encarga de devolver el nombre del jugador.
+    @Test
+    void testGetName() {
+        // Creamos un jugador con un nombre.
+        var player = new Player("TestPlayer", new HandCardList());
+        var immutablePlayer = new ImmutablePlayer(player);
+        // Comprobamos que el nombre del jugador es el mismo que el del jugador inmutable.
+        assertEquals(player.getName(), immutablePlayer.getName());
+    }
+}
